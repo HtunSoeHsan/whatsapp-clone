3f41eda36793b064dfa4bddffc9de6bf500d92d7
HtunSoeHsan <92197490+HtunSoeHsan@users.noreply.github.com>
2024-07-14 11:12:38 +0630

    feat: get conversation msg

--- convex/messages.ts
+++ convex/messages.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const sendTextMessage = mutation({
     args: {
@@ -22,5 +22,36 @@ export const sendTextMessage = mutation({
         await ctx.db.insert("messages", {sender: args.sender, content: args.content, conversation: args.conversation, messageType: "text"});
 
         // TODO: chat gpt message
+    }
+});
+
+export const getMessages = query({
+    args: { conversation: v.id("conversations")},
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) throw new ConvexError("Unauthorized");
+
+        const messages = await ctx.db.query("messages").withIndex("by_conversation", (q) => q.eq("conversation", args.conversation)).collect();
+
+        const userProfileCache = new Map();
+        const messagesWithSender = await Promise.all(
+            messages.map(async(message) => {
+                let sender;
+                if (userProfileCache.has(message.sender)) {
+                    sender = userProfileCache.get(message.sender);
+                } else {
+                sender = await ctx.db
+                  .query("users")
+                  .filter((q) => q.eq(q.field("_id"), message.sender))
+                  .first();
+
+                  userProfileCache.set(message.sender, sender);
+
+                }
+                return {...message, sender};
+            })
+        )
+
+        return messagesWithSender;
     }
 })
